2a1857ec59743a06519b317f8f8a65873b4ffd1c
@SarxRom <2233073251@correo.ler.uam.mx>
2025-02-18 11:02:53 -0600

    18 de febrero

--- variables/script.js
+++ variables/script.js
@@ -1,6 +1,9 @@
 // variables
 
-let ingrediente1 = "Azúcar";
-let costo = 39;
+let ingrediente1 = "Crema";
+let costo = 40;
 
-document.write(ingrediente1);
+//document.write(ingrediente1);
+document.getElementById('demo').innerHTML="Necesitamos de " + ingrediente1;
+document.getElementById('dem').innerHTML="El cual tiene un costo de: " + costo;
+document.getElementById('de').innerHTML="El precio del " + ingrediente1 + " es de: " + costo + ".";
