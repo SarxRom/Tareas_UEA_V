c64c7d8cc3c52977fdc8501e5aab7a6e4a286d9b
@SarxRom <2233073251@correo.ler.uam.mx>
2025-03-07 22:40:30 -0600

    tarea2

---
+++ tarea2.1/scrip.js
@@ -0,0 +1,10 @@
+let p1= "La tecnología web, representada por HTML, CSS y JavaScript, es esencial en mi carrera de Artes y Comunicaciones Digitales, ya que me permite fusionar la creatividad con lo técnico, creando experiencias interactivas y visuales dinámicas. Estas herramientas ofrecen la capacidad de diseñar obras que no solo se observan, sino que reaccionan a la interacción del usuario, lo que abre nuevas formas de expresión artística y me permite aplicar conceptos aprendidos en el aula, ya que me fomentando el pensamiento crítico y la resolución de problemas y habilidades que son claves en mi vida diaria así como en mis futuros proyectos.";
+
+let p2= "Sin embargo, las limitaciones de compatibilidad entre dispositivos y navegadores pueden restringir la libertad creativa, la programación técnica puede interferir con la espontaneidad artística y la tecnología web ofrece un enorme potencial para crear arte accesible y global, y su dominio me prepara para enfrentar los desafíos de un entorno digital que están en constante evolución.";
+
+document.getElementById('parr1').innerHTML=p1;
+
+document.getElementById('parr2').innerHTML=p2;
+
+console.log(p1.length);
+console.log(p2.length);
