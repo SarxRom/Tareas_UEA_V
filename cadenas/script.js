6782dd59a74491676f33b29cc19e033d19cf8af8
@SarxRom <2233073251@correo.ler.uam.mx>
2025-03-11 09:15:00 -0600

    11/03/2025

---
+++ cadenas/script.js
@@ -0,0 +1,6 @@
+let txt = "Estoy atrapada entre el ME QUIERO QUEDAR y el YA NO PUEDO";
+document.getElementById('cadena').innerHTML=txt;
+//console.log(txt.length);
+console.log(txt.length);
+
+//engrapar el texto es el atajo alt + z
